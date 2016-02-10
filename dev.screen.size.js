e48f927fbaba731486f54c03f6c578d8d7b2eec8
András <designer@mataiandras.com>
2016-02-10 20:39:10 +0100

    Create dev.screen.size.js

---
+++ dev.screen.size.js
@@ -0,0 +1,6 @@
+window.onresize = displayWindowSize;
+window.onload = displayWindowSize;
+function displayWindowSize() {
+    // your size calculation code here
+	console.log(window.innerWidth);
+};
